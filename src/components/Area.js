6e36769e9e520cd3435a7eabb0c5a4312d287c8d
Joshua Evans <skyfriends1@gmail.com>
2017-07-22 15:10:35 -0700

    finished styling the area comp

--- src/components/Area.js
+++ src/components/Area.js
@@ -1,21 +1,45 @@
 import React from 'react';
-// import Issue from './Issue';
+import Issue from './Issue';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 const generateKey = () => Math.random();
 
 const Area = props => (
-  <li>
-    <h2 className='area-name'>{props.area.areaName}</h2>
-    <table>
-      <tbody>
-      <tr>
-        <th></th>
-        <th>Condition</th>
-        <th>Pass/Fail</th>
-      </tr>
-      </tbody>
-    </table>
-  </li>
+  <div className='area-title'>
+  <MuiThemeProvider>
+  <Card>
+  <CardHeader
+    title={props.area.areaName}
+    actAsExpander={true}
+    showExpandableButton={true}
+  />
+  <CardText expandable={true}>
+  <MuiThemeProvider>
+    <Table>
+      <TableBody
+        displayRowCheckbox={false}>
+      <TableRow>
+        <TableHeaderColumn>Issue Name</TableHeaderColumn>
+        <TableHeaderColumn>Condition</TableHeaderColumn>
+        <TableHeaderColumn>Pass/Fail</TableHeaderColumn>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </MuiThemeProvider>
+  </CardText>
+  </Card>
+  </MuiThemeProvider>
+  </div>
 );
 
 export default Area;
