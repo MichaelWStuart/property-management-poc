fd9719ee99c969aa4243725252faa56f1c2209d8
Michael Stuart <mike.w.stuart@gmail.com>
2017-07-22 15:08:11 -0700

    put data in json, refactor shit

--- src/components/Area.js
+++ src/components/Area.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Issue from './Issue';
+import Issue from './Issue';
 
 const generateKey = () => Math.random();
 
@@ -8,11 +8,16 @@ const Area = props => (
     <h2 className='area-name'>{props.area.areaName}</h2>
     <table>
       <tbody>
-      <tr>
-        <th></th>
-        <th>Condition</th>
-        <th>Pass/Fail</th>
-      </tr>
+        <tr>
+          <th></th>
+          <th>Condition</th>
+          <th>Pass/Fail</th>
+        </tr>
+        {props.area.issues.map(issue =>
+          <Issue
+            key={generateKey()}
+            issue={issue} />
+        )}
       </tbody>
     </table>
   </li>
