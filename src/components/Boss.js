16f8f8afb989c01fea0db155ec810831ec77b04e
derekcfarmer <derek.farmer@gmail.com>
2017-07-22 21:37:51 -0700

    restyled buttons on tenant, manager, and boss

--- src/components/Boss.js
+++ src/components/Boss.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Areas from './Areas';
 import Header from './header';
 import data from '../../data.json';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Boss extends React.Component {
   constructor(props) {
@@ -20,7 +22,11 @@ export default class Boss extends React.Component {
         <Areas
           className='areas'
           areas={this.state.areas} />
-        <button>Approve</button>
+        <div style={{textAlign: 'center', margin: '2em'}}>
+          <MuiThemeProvider>
+            <RaisedButton href="/" label="Approve" labelColor={"#4476b2"} buttonStyle={{width: '30vw', border: '1px solid #4476b2', fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)'}}/>
+          </MuiThemeProvider>
+        </div>
       </div>
     );
   }
