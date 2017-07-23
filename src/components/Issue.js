f5b6f12dccea72758ff71df2c51a5e1818615e25
Michael Stuart <mike.w.stuart@gmail.com>
2017-07-22 21:03:49 -0700

    conditional logic for photo and text input

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -3,7 +3,7 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import Toggle from 'material-ui/Toggle';
-import TextField from 'material-ui/TextField'
+import TextField from 'material-ui/TextField';
 
 import {
   Table,
@@ -19,20 +19,29 @@ const generateKey = () => Math.random();
 class Issue extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {open: false, file: '', imagePreviewUrl: ''};
+    this.state = {
+      open: false,
+      file: '',
+      imagePreviewUrl: '',
+      value: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
     this.handleClose = this.handleClose.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
-    this.handlePhotoSubmit = this.handlePhotoSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handlePhotoSubmit(e) {
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
     e.preventDefault();
     this.setState({ open: false });
   }
 
   handleToggle() {
-    const newState = this.state.file ? {file: ''} : {open: !this.state.open};
+    const newState = this.state.file ? {file: '', imagePreviewUrl: '', value: ''} : {open: !this.state.open};
     this.setState(newState);
   }
 
@@ -89,12 +98,13 @@ class Issue extends React.Component {
       </RaisedButton>,
     ];
 
+    this.state.file && actions.pop();
     this.state.file && actions.push(
       <FlatButton
         key={generateKey()}
         label="Submit"
         primary={true}
-        onTouchTap={this.handlePhotoSubmit}
+        onTouchTap={this.handleSubmit}
         labelStyle={{color: '#4476b2'}}
       />);
 
@@ -119,9 +129,8 @@ class Issue extends React.Component {
         </TableRowColumn>
         {this.state.file && !this.state.open &&
           <TableRowColumn>
-            <div className="imgPreview">
-              {$imagePreview}
-            </div>
+            <div className="imgPreview">{$imagePreview}</div>
+            <div>{this.state.value}</div>
           </TableRowColumn>
         }
         <Dialog
@@ -138,6 +147,8 @@ class Issue extends React.Component {
             {$imagePreview}
           </div>
           <TextField
+            onChange={this.handleChange}
+            value={this.state.value}
             hintText="Comments"
             floatingLabelText="Describe comment"
             multiLine={true}
@@ -146,7 +157,7 @@ class Issue extends React.Component {
             floatingLabelStyle={{color: '#4476b2'}}
             floatingLabelFocusStyle={{color: '#4476b2'}}
             underlineFocusStyle={{borderColor: '#4476b2'}}
-            />
+          />
 
         </Dialog>
       </TableRow>
