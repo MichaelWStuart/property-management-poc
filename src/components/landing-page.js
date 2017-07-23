6fe8b592557d9539341d0fac51c409894aac6412
derekcfarmer <derek.farmer@gmail.com>
2017-07-22 21:11:24 -0700

    button styling

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -24,7 +24,7 @@ class Landing extends React.Component{
           <img src='https://github.com/skyfriends/podpodpod/blob/master/src/components/pod-logo-smaller.png?raw=true' style={{height: '30vh'}}/>
         </div>
         <MuiThemeProvider>
-          <RaisedButton href="/manager" label="Welcome" buttonStyle={{fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)', position: 'fixed', top: '70%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}/>
+          <RaisedButton href="/tenant" label="Welcome" buttonStyle={{borderRadius: '5px', border: '1px solid #4476b2', fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)', position: 'fixed', top: '70%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}/>
         </MuiThemeProvider>
       </div>
     );
