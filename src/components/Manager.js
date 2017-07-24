7d64749ff244772d0d3d9a4512b43931c9f53416
Michael Stuart <mike.w.stuart@gmail.com>
2017-07-24 14:35:57 -0700

    button properly nested

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -4,7 +4,6 @@ import Header from './header';
 import Footer from './footer';
 import MediaQuery from 'react-responsive';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
 import data from '../../data.json';
 import TenantContainer from './tenant-container.js';
 const generateKey = () => Math.random();
@@ -25,11 +24,6 @@ export default class Manager extends React.Component {
         </MediaQuery>
         <div style={{paddingBottom: '4em'}}>
           {this.state.tenants.map((tenant, tenantIndex) => <TenantContainer tenantIndex={tenantIndex} areas={this.state.areas} tenant={tenant} key={generateKey()} />)}
-          <div style={{textAlign: 'center', margin: '2em'}}>
-            <MuiThemeProvider>
-              <RaisedButton href="/boss" label="Approve" labelColor={'#4476b2'} buttonStyle={{width: '30vw', border: '1px solid #4476b2', fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)'}}/>
-            </MuiThemeProvider>
-          </div>
         </div>
         <Footer />
       </div>
