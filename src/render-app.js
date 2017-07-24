a4d305ef7cf2d173940e547a7cd244507b728f82
derekcfarmer <derek.farmer@gmail.com>
2017-07-24 10:38:50 -0700

    pre-merge commit

--- src/render-app.js
+++ src/render-app.js
@@ -5,7 +5,7 @@ const renderApp = () =>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <script src="https://use.fontawesome.com/b329f349e9.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-      <title>jsk pm</title>
+      <title>aPodments Property Management</title>
     </head>
     <body>
       <div id="root"></div>
