83365d8b66b74397aaa5fe3bde7d1dae7c603109
derekcfarmer <derek.farmer@gmail.com>
2017-07-25 10:33:43 -0700

    added media query for blue background on landing screen

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import RaisedButton from 'material-ui/RaisedButton';
 import PasswordForm from './password-form';
 import { withRouter } from 'react-router-dom';
@@ -37,14 +38,29 @@ class Landing extends React.Component{
 
   render(){
     return (
-      <div style={{height: '100vh', overflow: 'hidden', width: 'auto', backgroundImage: 'radial-gradient(circle, rgb(182, 202, 222), rgb(51,87,129)'}}>
-        <div style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}>
-          <img src='https://s3-us-west-2.amazonaws.com/podpodpod/pod-logo-smaller.png' style={{height: '30vh'}}/>
-        </div>
-        {this.state.triggered && <PasswordForm testPassword={this.testPassword}/>}
-        <MuiThemeProvider>
-          <RaisedButton onClick={this.handleButtonClick} label="Welcome Home" labelColor={'#4476b2'} buttonStyle={{border: '1px solid #4476b2', fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)', position: 'fixed', top: '70%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}/>
-        </MuiThemeProvider>
+      <div>
+        <MediaQuery query='(min-device-width: 1224px)'>
+          <div style={{height: '100vh', overflow: 'hidden', width: 'auto', backgroundImage: 'radial-gradient(circle, rgb(182, 202, 222), rgb(51,87,129)'}}>
+            <div style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}>
+              <img src='https://s3-us-west-2.amazonaws.com/podpodpod/pod-logo-smaller.png' style={{height: '30vh'}}/>
+            </div>
+            {this.state.triggered && <PasswordForm testPassword={this.testPassword}/>}
+            <MuiThemeProvider>
+              <RaisedButton onClick={this.handleButtonClick} label="Welcome Home" labelColor={'#4476b2'} buttonStyle={{border: '1px solid #4476b2', fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)', position: 'fixed', top: '70%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}/>
+            </MuiThemeProvider>
+          </div>
+        </MediaQuery>
+        <MediaQuery query='(max-device-width: 1223px)'>
+          <div style={{height: '100vh', overflow: 'hidden', width: 'auto'}}>
+            <div style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}>
+              <img src='https://s3-us-west-2.amazonaws.com/podpodpod/pod-logo-smaller.png' style={{height: '30vh'}}/>
+            </div>
+            {this.state.triggered && <PasswordForm testPassword={this.testPassword}/>}
+            <MuiThemeProvider>
+              <RaisedButton onClick={this.handleButtonClick} label="Welcome Home" labelColor={'#4476b2'} buttonStyle={{border: '1px solid #4476b2', fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)', position: 'fixed', top: '70%', left: '50%', transform: 'translate(-50%, -50%)', width: 'auto'}}/>
+            </MuiThemeProvider>
+          </div>
+      </MediaQuery>
       </div>
     );
   }
